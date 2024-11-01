e764c2e44d6a03893fc723ff6a13f0a369bb2012
Guillem Alqueza Mayench <guillem.alqueza@estudiantat.upc.edu>
2024-11-01 21:56:01 +0100

    Fix logger

--- Engine/Engine/Logger.cpp
+++ Engine/Engine/Logger.cpp
@@ -19,7 +19,7 @@ void Logger::Log(const char file[], int line, LogType type, const char* format, 
     va_start(ap, format);
     vsprintf_s(tmpString1, sizeof(tmpString1), format, ap);
     va_end(ap);
-    sprintf_s(tmpString2, sizeof(tmpString2), "%s(%d) : %s", filename, line, tmpString1);
+    sprintf_s(tmpString2, sizeof(tmpString2), "\n%s(%d) : %s", filename, line, tmpString1);
     OutputDebugStringA(tmpString2);
 
     logger.AddLog(type, tmpString2);
@@ -27,6 +27,7 @@ void Logger::Log(const char file[], int line, LogType type, const char* format, 
 
 void Logger::AddLog(LogType type, std::string message)
 {
+    message.erase(std::remove(message.begin(), message.end(), '\n'), message.end());
 	logs.push_back({ type, message });
 }
 
