e87a1ccba0dc2ce19b275ddc0e879817f7c20d61
Lluccib <lluc.cid@estudiantat.upc.edu>
2024-11-10 21:19:14 +0100

    About window

--- Engine/Engine/AboutWindow.cpp
+++ Engine/Engine/AboutWindow.cpp
@@ -14,5 +14,58 @@ void AboutWindow::DrawWindow()
 
 	UpdateMouseState();
 
-	ImGui::End();
+    // Title
+    ImGui::Text("SpectraEngine");
+    ImGui::Separator();
+
+    // Description
+    ImGui::Text("Description");
+    ImGui::TextWrapped("SpectraEngine is a 3D game engine developed in C++ with OpenGL. It can render geometry with controls and information displays.");
+    ImGui::TextWrapped("GitHub: ");
+	ImGui::SameLine();
+    ImGui::TextWrapped("https://github.com/CITM-UPC/SpectraEngine");
+
+    ImGui::Separator();
+
+    // Team Members
+    ImGui::Text("Team Members");
+    ImGui::BulletText("Guillem Alqueza Mayench: https://github.com/guillemalqueza");
+    ImGui::BulletText("Miguel Iglesias Abarca: https://github.com/MiguelIglesiasAbarca");
+    ImGui::BulletText("Lluc Cid: https://github.com/Lluccib");
+
+    ImGui::Separator();
+
+    // Viewer Controls
+    ImGui::Text("Viewer Controls");
+    ImGui::BulletText("Right-click: Move the current view.");
+    ImGui::BulletText("Right-click + WASD: Fly around the scene.");
+    ImGui::BulletText("Alt + Right-click or Mouse Wheel: Zoom in or out.");
+    ImGui::BulletText("Alt + Left-click: Orbit the camera around a pivot point.");
+    ImGui::BulletText("Alt + Mouse Wheel: Pan the camera.");
+    ImGui::BulletText("F: Frame the selected object.");
+
+    ImGui::Separator();
+
+    // Libraries and Dependencies
+    ImGui::Text("Libraries and Dependencies");
+    ImGui::BulletText("SDL2:");
+    ImGui::SameLine();
+    ImGui::TextWrapped("https://github.com/libsdl-org/SDL");
+    ImGui::BulletText("OpenGL:");
+    ImGui::SameLine();
+    ImGui::TextWrapped("https://www.opengl.org/");
+    ImGui::BulletText("ImGui:");
+    ImGui::SameLine();
+    ImGui::TextWrapped("https://github.com/ocornut/imgui");
+    ImGui::BulletText("GLM:");
+    ImGui::SameLine();
+    ImGui::TextWrapped("https://github.com/g-truc/glm");
+    ImGui::BulletText("Assimp:");
+    ImGui::SameLine();
+    ImGui::TextWrapped("https://github.com/assimp/assimp");
+    ImGui::BulletText("DeviL:");
+    ImGui::SameLine();
+    ImGui::TextWrapped("https://github.com/DentonW/DevIL");
+
+    ImGui::End();
 }
